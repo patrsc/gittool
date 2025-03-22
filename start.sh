341df348e82a395d3c151e816f679cd825194c9f
Patrick <patrsc@nubu.at>
2025-03-22 20:12:36 +0100

    Update shell script

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Path to python
+python_path="python"
+
+# Path to gittool
+gittool_path="."
+
 # Set the port number
 PORT=15080
 
 # Start the Python server in the background (hide output)
-nohup python3 server.py >/dev/null 2>&1 &
+nohup "$python_path" "$gittool_path/server.py" >/dev/null 2>&1 &
 
 # Wait a moment to ensure the server starts
 sleep 0.5  # seconds
